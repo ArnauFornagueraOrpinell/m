a62e5df0e09e25033750973c38746d1e38b990ae
Galiat <arnaufornaguera@gmail.com>
2025-01-16 00:53:54 +0100

    a

--- madersenia_cli/src/components/ProductComponent.vue
+++ madersenia_cli/src/components/ProductComponent.vue
@@ -6,173 +6,191 @@
   >
     <!-- Header con información principal -->
     <q-card-section class="bg-primary text-white">
-      <div class="row q-col-gutter-md">
-        <div class="col-12 col-sm-6">
-          <div class="text-h6">Producto #{{ model.CODI_PRODUCTE }}</div>
-          <div class="text-subtitle2">Ref: {{ model.PRODUCT_ID }}</div>
-        </div>
-        <div class="col-12 col-sm-6 text-right">
-          <q-badge 
-            :color="editable ? 'positive' : 'grey'" 
-            class="q-pa-sm"
-          >
-            {{ editable ? 'Modo Edición' : 'Solo Lectura' }}
-          </q-badge>
-        </div>
-      </div>
-    </q-card-section>
-
-    <!-- Contenido principal -->
-    <q-card-section>
-      <div class="row q-col-gutter-md">
-        <!-- Identificación del producto -->
-        <div class="col-12 col-md-6">
-          <q-input
-            v-model="model.CODI_PRODUCTE"
-            @input="updateValue('CODI_PRODUCTE')"
-            type="number"
-            label="CODI_PRODUCTE"
+      <div class="row items-center q-col-gutter-md">
+        <!-- Toggle collapse button -->
+        <div class="col-auto">
+          <q-btn
+            flat
+            round
             dense
-            outlined
-            :readonly="!editable"
-            class="q-mb-md"
-          />
-          <q-input
-            v-model="model.PRODUCT_ID"
-            @input="updateValue('PRODUCT_ID')"
-            label="Referencia"
-            dense
-            outlined
-            :readonly="!editable"
-            class="q-mb-md"
+            :icon="isCollapsed ? 'arrow_right' : 'arrow_drop_down'"
+            @click="toggleCollapse"
           />
         </div>
-
-        <!-- Descripción -->
-        <div class="col-12 col-md-6">
-          <q-input
-            v-model="model.DESCRIPCIO"
-            @input="updateValue('DESCRIPCIO')"
-            label="Descripción"
-            type="textarea"
-            dense
-            outlined
-            :readonly="!editable"
-            class="q-mb-md"
-          />
-        </div>
-
-        <!-- Dimensiones -->
-        <div class="col-12">
-          <q-card-section class="text-subtitle2 q-pb-none">
-            Dimensiones
-          </q-card-section>
-          <div class="row q-col-gutter-md">
-            <div class="col-12 col-sm-4">
-              <q-input
-                v-model="model.ANCHO"
-                @input="updateValue('ANCHO')"
-                type="number"
-                label="Ancho"
-                dense
-                outlined
-                :readonly="!editable"
-                class="q-mb-md"
+        <div class="col">
+          <div class="row q-col-gutter-md items-center">
+            <div class="col-12 col-sm-6">
+              <div class="text-h6">Producto #{{ model.CODI_PRODUCTE }}</div>
+              <div class="text-subtitle2">Ref: {{ model.PRODUCT_ID }}</div>
+            </div>
+            <div class="col-12 col-sm-6 text-right">
+              <q-badge 
+                :color="editable ? 'positive' : 'grey'" 
+                class="q-pa-sm"
               >
-                <template v-slot:append>
-                  <div class="text-grey-7">mm</div>
-                </template>
-              </q-input>
-            </div>
-            <div class="col-12 col-sm-4">
-              <q-input
-                v-model="model.LARGO"
-                @input="updateValue('LARGO')"
-                type="number"
-                label="Largo"
-                dense
-                outlined
-                :readonly="!editable"
-                class="q-mb-md"
-              >
-                <template v-slot:append>
-                  <div class="text-grey-7">mm</div>
-                </template>
-              </q-input>
-            </div>
-            <div class="col-12 col-sm-4">
-              <q-input
-                v-model="model.GRUESO"
-                @input="updateValue('GRUESO')"
-                type="number"
-                label="Grueso"
-                dense
-                outlined
-                :readonly="!editable"
-                class="q-mb-md"
-              >
-                <template v-slot:append>
-                  <div class="text-grey-7">mm</div>
-                </template>
-              </q-input>
-            </div>
-          </div>
-        </div>
-
-        <!-- Ubicación y embalaje -->
-        <div class="col-12">
-          <q-card-section class="text-subtitle2 q-pb-none">
-            Ubicación y Embalaje
-          </q-card-section>
-          <div class="row q-col-gutter-md">
-            <div class="col-12 col-sm-6">
-              <q-input
-                v-model="model.TIPUS_EMBALATGE"
-                @input="updateValue('TIPUS_EMBALATGE')"
-                label="Tipus Embalatge"
-                dense
-                outlined
-                :readonly="!editable"
-                class="q-mb-md"
-              />
-            </div>
-            <div class="col-12 col-sm-6">
-              <q-input
-                v-model="model.UBICACIO_1"
-                @input="updateValue('UBICACIO_1')"
-                label="Edificio"
-                dense
-                outlined
-                :readonly="!editable"
-                class="q-mb-md"
-              />
-            </div>
-            <div class="col-12 col-sm-6">
-              <q-input
-                v-model="model.UBICACIO_2"
-                @input="updateValue('UBICACIO_2')"
-                label="Planta"
-                dense
-                outlined
-                :readonly="!editable"
-                class="q-mb-md"
-              />
-            </div>
-            <div class="col-12 col-sm-6">
-              <q-input
-                v-model="model.UBICACIO_3"
-                @input="updateValue('UBICACIO_3')"
-                label="Habitación"
-                dense
-                outlined
-                :readonly="!editable"
-                class="q-mb-md"
-              />
+                {{ editable ? 'Modo Edición' : 'Solo Lectura' }}
+              </q-badge>
             </div>
           </div>
         </div>
       </div>
     </q-card-section>
+
+    <!-- Contenido principal - Collapsible -->
+    <q-slide-transition>
+      <div v-show="!isCollapsed">
+        <q-card-section>
+          <div class="row q-col-gutter-md">
+            <!-- Identificación del producto -->
+            <div class="col-12 col-md-6">
+              <q-input
+                v-model="model.CODI_PRODUCTE"
+                @input="updateValue('CODI_PRODUCTE')"
+                type="number"
+                label="CODI_PRODUCTE"
+                dense
+                outlined
+                :readonly="!editable"
+                class="q-mb-md"
+              />
+              <q-input
+                v-model="model.PRODUCT_ID"
+                @input="updateValue('PRODUCT_ID')"
+                label="Referencia"
+                dense
+                outlined
+                :readonly="!editable"
+                class="q-mb-md"
+              />
+            </div>
+
+            <!-- Descripción -->
+            <div class="col-12 col-md-6">
+              <q-input
+                v-model="model.DESCRIPCIO"
+                @input="updateValue('DESCRIPCIO')"
+                label="Descripción"
+                type="textarea"
+                dense
+                outlined
+                :readonly="!editable"
+                class="q-mb-md"
+              />
+            </div>
+
+            <!-- Dimensiones -->
+            <div class="col-12">
+              <q-card-section class="text-subtitle2 q-pb-none">
+                Dimensiones
+              </q-card-section>
+              <div class="row q-col-gutter-md">
+                <div class="col-12 col-sm-4">
+                  <q-input
+                    v-model="model.ANCHO"
+                    @input="updateValue('ANCHO')"
+                    type="number"
+                    label="Ancho"
+                    dense
+                    outlined
+                    :readonly="!editable"
+                    class="q-mb-md"
+                  >
+                    <template v-slot:append>
+                      <div class="text-grey-7">mm</div>
+                    </template>
+                  </q-input>
+                </div>
+                <div class="col-12 col-sm-4">
+                  <q-input
+                    v-model="model.LARGO"
+                    @input="updateValue('LARGO')"
+                    type="number"
+                    label="Largo"
+                    dense
+                    outlined
+                    :readonly="!editable"
+                    class="q-mb-md"
+                  >
+                    <template v-slot:append>
+                      <div class="text-grey-7">mm</div>
+                    </template>
+                  </q-input>
+                </div>
+                <div class="col-12 col-sm-4">
+                  <q-input
+                    v-model="model.GRUESO"
+                    @input="updateValue('GRUESO')"
+                    type="number"
+                    label="Grueso"
+                    dense
+                    outlined
+                    :readonly="!editable"
+                    class="q-mb-md"
+                  >
+                    <template v-slot:append>
+                      <div class="text-grey-7">mm</div>
+                    </template>
+                  </q-input>
+                </div>
+              </div>
+            </div>
+
+            <!-- Ubicación y embalaje -->
+            <div class="col-12">
+              <q-card-section class="text-subtitle2 q-pb-none">
+                Ubicación y Embalaje
+              </q-card-section>
+              <div class="row q-col-gutter-md">
+                <div class="col-12 col-sm-6">
+                  <q-input
+                    v-model="model.TIPUS_EMBALATGE"
+                    @input="updateValue('TIPUS_EMBALATGE')"
+                    label="Tipus Embalatge"
+                    dense
+                    outlined
+                    :readonly="!editable"
+                    class="q-mb-md"
+                  />
+                </div>
+                <div class="col-12 col-sm-6">
+                  <q-input
+                    v-model="model.UBICACIO_1"
+                    @input="updateValue('UBICACIO_1')"
+                    label="Edificio"
+                    dense
+                    outlined
+                    :readonly="!editable"
+                    class="q-mb-md"
+                  />
+                </div>
+                <div class="col-12 col-sm-6">
+                  <q-input
+                    v-model="model.UBICACIO_2"
+                    @input="updateValue('UBICACIO_2')"
+                    label="Planta"
+                    dense
+                    outlined
+                    :readonly="!editable"
+                    class="q-mb-md"
+                  />
+                </div>
+                <div class="col-12 col-sm-6">
+                  <q-input
+                    v-model="model.UBICACIO_3"
+                    @input="updateValue('UBICACIO_3')"
+                    label="Habitación"
+                    dense
+                    outlined
+                    :readonly="!editable"
+                    class="q-mb-md"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </q-card-section>
+      </div>
+    </q-slide-transition>
   </q-card>
 </template>
 
@@ -198,11 +216,16 @@ export default {
   emits: ['update:modelValue'],
   setup(props, { emit }) {
     const model = ref(props.modelValue);
+    const isCollapsed = ref(false);
 
     const updateValue = (key) => {
       if (props.editable) {
         emit('update:modelValue', { ...model.value, [key]: model.value[key] });
       }
+    }
+
+    const toggleCollapse = () => {
+      isCollapsed.value = !isCollapsed.value;
     }
 
     watch(() => props.modelValue, (newVal) => {
@@ -211,7 +234,9 @@ export default {
 
     return {
       model,
-      updateValue
+      isCollapsed,
+      updateValue,
+      toggleCollapse
     }
   }
 };
