4910761e67bd4b3def93a107721fffecb3362def
Galiat <arnaufornaguera@gmail.com>
2025-01-16 00:27:42 +0100

    a

--- madersenia_api/index.js
+++ madersenia_api/index.js
@@ -547,6 +547,43 @@ app.get('/get-product-by-barcode', (req, res) => {
   }
 });
 
+app.get('/get-last-product', (req, res) => {
+  let conn;
+  try {
+    console.log("Attempting to connect: " + connStr);
+    conn = ibmdb.openSync(connStr);
+    console.log("Connected to: " + DATABASE);
+
+    let query = `
+      SELECT CODI_PRODUCTE, DESCRIPCIO, NUM_DOCUMENT_OF, EXERCICI_OF 
+      FROM EMP1.VGESCO_QUERY_ETQ_TAL3 
+      WHERE NUM_DOCUMENT_OF = (
+        SELECT MAX(NUM_DOCUMENT_OF) 
+        FROM EMP1.VGESCO_QUERY_ETQ_TAL3
+        WHERE EXERCICI_OF = (
+          SELECT MAX(EXERCICI_OF) 
+          FROM EMP1.VGESCO_QUERY_ETQ_TAL3
+        )
+      )`;
+    
+    const data = conn.querySync(query);
+    console.log("Last product added:", data);
+
+    res.status(200).json(data[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  } finally {
+    if (conn) {
+      try {
+        conn.closeSync();
+        console.log("Connection closed");
+      } catch (err) {
+        console.log("Error closing connection:", err);
+      }
+    }
+  }
+});
+
 app.get('/get-columns', (req, res) => {
   let conn;
   try {
